84d9987852b5ef50ba2593c1f38dd14c1bfec1da
Mayank Jaat <jatmayankjatm25@gmail.com>
2024-08-06 22:39:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7335128385:AAFudmBJ9MDyipGp1MOvHJbsKLoCgVmzPJY"
 OWNER_ID = int(getenv("OWNER_ID", "5222155765"))
 MONGODB = getenv("MONGO_DB", "mongodb+srv://testbot:testbot@cluster0.7rzwxwx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1001917606160"))
-CHANNEL_ID = int(getenv("CHANNEL_ID", "WarriorUnitsBots"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002023786492"))
